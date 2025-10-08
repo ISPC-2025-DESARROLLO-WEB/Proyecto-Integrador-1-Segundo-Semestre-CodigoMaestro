61a8677cc6d30a5af2b845fd94c91b167590ddbd
guadalupeturri <magaliturri@gmail.com>
2025-10-08 18:44:51 -0300

    Realizamos validaciones de la pagina contacto y creamos archivo JS

---
+++ AgroSilos_Control/Front/js/validaciones.js
@@ -0,0 +1,15 @@
+// Validación personalizada Bootstrap + envío simulado
+document.getElementById('contactoForm').addEventListener('submit', function(e) {
+  e.preventDefault(); // Evita el envío por defect
+  
+  const form = e.target;
+  
+  if (!form.checkValidity()) {
+    e.stopPropagation();
+    form.classList.add('was-validated');
+  } else {
+    alert('✅ Mensaje enviado correctamente. ¡Gracias por contactarnos!');
+    form.reset();
+    form.classList.remove('was-validated');
+  }
+});
